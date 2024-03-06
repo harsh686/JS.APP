f503e4eee562b6076862b0898a2646600b9fb429
HARSHVARDHAN SINGH <129592534+harsh686@users.noreply.github.com>
2024-03-06 08:16:34 +0000

    conversion of datatypes

--- basics.js/datatypes.js
+++ basics.js/datatypes.js
@@ -1,2 +1,17 @@
-console.log(typeof null);
-console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+let score = "33abc"
+// console.log(typeof score);
+
+// let valueinNumber = Number(score);
+// console.log(valueinNumber);
+
+let loggedIn = 1;
+let booleanloggedIn = Boolean(loggedIn);
+// console.log(booleanloggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
