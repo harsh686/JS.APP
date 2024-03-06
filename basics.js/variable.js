c5938a162235c81e537978e15fac468237d53dea
HARSHVARDHAN SINGH <129592534+harsh686@users.noreply.github.com>
2024-03-06 07:12:22 +0000

    changes made

---
+++ basics.js/variable.js
@@ -0,0 +1,24 @@
+const accountId = 23456
+let accountEmail = "harsh76545@gmail.com"
+var accountPassword = "4567"
+accountCity = "dhanbad"
+// console.log([accountEmail,accountId,accountPassword,accountCity])
+
+accountEmail = "h@gmail.com";
+accountPassword="5678"
+accountCity ="jaipur";
+let accountState;
+
+// /console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+// function calSum(a,b){
+//     let sum = a +b;
+   
+// }
+// calSum(a,b)
+// console.log(sum);
+
+let name = "harsh"
+ let age = 3;
+ let voteAge = true
+ console.log(typeof(age));
